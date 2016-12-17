85408eb80b967a80acd6287208a0032423966976
dharamvirsaini <honeysaini88@gmail.com>
2016-12-17 12:24:30 +0530

    Update OpenTokConfig.java

--- app/src/main/java/com/example/honey/screensharing/OpenTokConfig.java
+++ app/src/main/java/com/example/honey/screensharing/OpenTokConfig.java
@@ -5,10 +5,10 @@ public class OpenTokConfig {
     // ***                      https://dashboard.tokbox.com/projects                           ***
 
     // Replace with your OpenTok API key
-    public static final String API_KEY = "45302582";
+    public static final String API_KEY = "YOUR_API_KEY";
     // Replace with a generated Session ID
-    public static final String SESSION_ID = "1_MX40NTMwMjU4Mn5-MTQ3OTgwOTUxMzk1Nn5Nc1ZMUFd0Q0ozY1Q2MU9oUlNia3dOcGp-fg";
+    public static final String SESSION_ID = "YOUR_SESSION_ID";
     public static final boolean SUBSCRIBE_TO_SELF = false;
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTMwMjU4MiZzaWc9YTRiYmUwYjBkZjVjYTMyZmEwZmRjODY0MzhmMTMyYzM1ZmQ3MmEzODpzZXNzaW9uX2lkPTFfTVg0ME5UTXdNalU0TW41LU1UUTNPVGd3T1RVeE16azFObjVOYzFaTVVGZDBRMG96WTFRMk1VOW9VbE5pYTNkT2NHcC1mZyZjcmVhdGVfdGltZT0xNDc5ODA5NTQ0Jm5vbmNlPTAuODgyNjQ2OTY5MDc0MDAyNiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDgyNDAxNTQz";
+    public static final String TOKEN = "YOUR_TOKEN";
 }
